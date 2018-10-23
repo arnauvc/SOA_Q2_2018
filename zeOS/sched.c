b6fd137e9cadf9938445b30e78975b0c42ba6af9
0xCarl <upc.carlestornel@gmail.com>
2018-10-23 20:22:37 +0200

    update mask

--- zeOS/sched.c
+++ zeOS/sched.c
@@ -28,8 +28,8 @@ struct task_struct *list_head_to_task_struct(struct list_head *l)
 #endif
 
 struct task_struct *list_head_to_task_struct(struct list_head *l){
-  struct task_struct *p;
-  p = ((struct task_struct *)((char *)(l)-(unsigned long)(&((struct task_struct *)0xfffff000)->list)));
+  unsigned int value = ( (unsigned int)((*l))&(0xfffff000) );
+  struct task_struct *p = (struct task_struct *)value;
   return p;
 }
 
